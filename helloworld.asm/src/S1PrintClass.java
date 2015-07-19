0e6fd054f4fc141a6aefa0d5d194bb17c13907c9
scouter-project <scouter.project@gmail.com>
2015-07-20 01:30:33 +0900

    refactoring

--- helloworld.asm/src/S1PrintClass.java
+++ helloworld.asm/src/S1PrintClass.java
@@ -1,3 +1,19 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ *  
+ *  @author skyworker
+ */
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -7,8 +23,10 @@ import org.objectweb.asm.ClassVisitor;
 import org.objectweb.asm.Opcodes;
 
 public class S1PrintClass {
+	private static final String TMP_HELLO_WORLD_CLASS = "/tmp/HelloWorld.class";
+
 	public static void main(String[] args) throws IOException {
-		ClassReader cr = new ClassReader(getClassBytes());
+		ClassReader cr = new ClassReader(load());
 		cr.accept(new ClassVisitor(Opcodes.ASM4) {
 			public void visit(int version, int access, String name, String signature, String superName,
 					String[] interfaces) {
@@ -19,8 +37,8 @@ public class S1PrintClass {
 
 	}
 
-	private static byte[] getClassBytes() throws IOException {
-		File f = new File("/tmp/HelloWorld.class");
+	private static byte[] load() throws IOException {
+		File f = new File(TMP_HELLO_WORLD_CLASS);
 		FileInputStream r = new FileInputStream(f);
 		byte[] buff = new byte[(int) f.length()];
 		r.read(buff);
--- helloworld.asm/src/S2PrintMathod.java
+++ helloworld.asm/src/S2PrintMathod.java
@@ -1,3 +1,18 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License"); 
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. 
+ *  
+ *  @author skyworker
+ */
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -9,15 +24,17 @@ import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
 public class S2PrintMathod {
+	private static final String TMP_HELLO_WORLD_CLASS = "/tmp/HelloWorld.class";
+
 	public static void main(String[] args) throws IOException {
 		ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
-		ClassReader cr = new ClassReader(getClassBytes());
+		ClassReader cr = new ClassReader(load());
 		cr.accept(new S2MyClassVisitor(cw), 0);
 		byte[] newClass = cw.toByteArray();
 	}
 
-	private static byte[] getClassBytes() throws IOException {
-		File f = new File("/tmp/HelloWorld.class");
+	private static byte[] load() throws IOException {
+		File f = new File(TMP_HELLO_WORLD_CLASS);
 		FileInputStream r = new FileInputStream(f);
 		byte[] buff = new byte[(int) f.length()];
 		r.read(buff);
